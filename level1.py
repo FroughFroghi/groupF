338112565f4ffb717e9b7dc0011b622a23281cab
Viola <zcqavet@users.noreply.github.com>
2016-10-26 19:22:17 +0100

    Update level1.py

--- level1.py
+++ level1.py
@@ -1,32 +1,40 @@
 
-answer1= raw_input("what is the answer to question 1?")
-answer2= raw_input("what is the answer to question 2?")
-answer3= raw_input("what is the answer to question 3?")
-answer4= raw_input("what is the answer to question 4?")
-answer5= raw_input("what is the answer to question 5?")
+answer1= int(input("what is the answer to question 1?"))
+answer2= int(input("what is the answer to question 2?"))
+answer3= int(input("what is the answer to question 3?"))
+answer4= int(input("what is the answer to question 4?"))
+answer5= int(input("what is the answer to question 5?"))
 
 if answer1 == 3:
-    print "Your answer to question 1 is correct. Well done!" #this doesn't work; and if you give the correct answer 3 it still says "incorrect", I don't know why! help!!!
+    print "Your answer to question 1 is correct. Well done!" 
 if answer1 >3:
 	print "Your answer to question 1 is incorrect."
 if answer1 <3:
 	print "Your answer to question 1 is incorrect."
-
+	
+if answer1 == 17:
+    print "Your answer to question 1 is correct. Well done!"
 if answer2 >17:
 	print "Your answer to question 2 is incorrect."
 if answer2 <17:
 	print "Your answer to question 2  is incorrect."
-
+	
+if answer1 == 1.5:
+    print "Your answer to question 1 is correct. Well done!"
 if answer3 > 1.5:
     print "Your answer to question 3 is incorrect."
 if answer3 < 1.5:
     print "Your answerto question 3 is incorrect."
 
+if answer1 == 14:
+    print "Your answer to question 1 is correct. Well done!"
 if answer4 >14:
 	print "Your answer to question 4 is incorrect."
 if answer4 <14:
 	print "Your answerto question 4 is incorrect."
 
+if answer1 == 14:
+    print "Your answer to question 1 is correct. Well done!"
 if answer5 >14:
 	print "Your answer to question 5 is incorrect."
 if answer5 <14:
