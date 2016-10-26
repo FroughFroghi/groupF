b060cd236019ae3e3b53b381d0e947303807faa4
zcqavet <Viola@violas-air.home>
2016-10-26 22:32:08 +0100

    added level1.html and level1.py
    i think it’s best to full get level1.html and level1.py running. - then
    we just copy and paste to the other 2 pages

--- level1.py
+++ level1.py
@@ -1,3 +1,22 @@
+#the flask part is copied form the internet
+from flask import Flask, render_template, request
+
+@app.route('/level1')
+def quiz():
+    return render_template('level1.html')
+
+@app.route('/quiz_answers', methods=['POST'])
+def quiz_answers():
+    q1 = request.form['q1']
+    q2 = request.form['q2']
+    q4 = request.form['q4']
+    q5 = request.form['q5']
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host='0.0.0.0')
+
+#the part below works in pycharm; it's functional; but I am struggeling to connect this to the input the html is collecting
 
 answer1= int(input("what is the answer to question 1?"))
 answer2= int(input("what is the answer to question 2?"))
@@ -39,3 +58,4 @@ if answer5 >14:
 	print "Your answer to question 5 is incorrect."
 if answer5 <14:
 	print "Your answer to question 5 is incorrect."
+
